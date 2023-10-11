81278a97846c38dde1f7f579039ac0f5394a6252
Lycik4546 <147642762+Lycik4546@users.noreply.github.com>
2023-10-11 20:50:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm tema')
+    print_hi('PyCharmh tema')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
